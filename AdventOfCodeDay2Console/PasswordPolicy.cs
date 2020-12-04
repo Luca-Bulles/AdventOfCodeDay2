76d905cc25968c2d1c7e267aa56168ec2ec8f9bd
Luca Bulles <lucabulles@gmail.com>
2020-12-04 14:45:30 +0100

    Added PasswordPolicy class with lists

--- AdventOfCodeDay2Console/PasswordPolicy.cs
+++ AdventOfCodeDay2Console/PasswordPolicy.cs
@@ -4,7 +4,15 @@ using System.Text;
 
 namespace AdventOfCodeDay2Console
 {
-    class PasswordPolicy
+    //Getal 1 geeft de minimale waarde aan
+    //Getal 2 geeft de maximale waarde aan
+    //De eerste lettter geeft aan welke letter gebruikt moet worden
+    //Achter : is de input en moet gecontroleerd worden
+    public class PasswordPolicy
     {
+        List<string> MinimalNumber = new List<string>();
+        List<string> MaximalNumber = new List<string>();
+        List<string> RequiredLetter = new List<string>();
+        List<string> Passwords = new List<string>();
     }
 }
