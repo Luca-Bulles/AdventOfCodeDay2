66f717ffa194006b083b1a3449e018927df8286f
Luca Bulles <lucabulles@gmail.com>
2021-01-12 14:19:52 +0100

    Added testmethods about adding itmes from textfile to lists

---
+++ AdventOfCodeDay2Console.Tests/AreItemsAddedToLists.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCodeDay2Console.Tests
+{
+    [TestClass]
+    public class AreItemsAddedToLists
+    {
+        [TestMethod]
+        public void MinimalNumberValuesAreAddedToList()
+        {
+            //Arrange
+            Database database = new Database();
+
+            //Act
+            database.ReadTextFile();
+            //Assert 
+            Assert.IsNotNull(database.TextFileMinimalNumber);
+            Assert.AreEqual(3, database.TextFileMinimalNumber[0]);
+        }
+        [TestMethod]
+        public void MaximalNumberValuesAreAddedToList()
+        {
+            //Arrange
+            Database database = new Database();
+
+            //Act
+            database.ReadTextFile();
+            //Assert 
+            Assert.IsNotNull(database.TextFileMaximalNumber);
+            Assert.AreEqual(4, database.TextFileMaximalNumber[0]);
+        }
+        [TestMethod]
+        public void RequiredLetterValuesAreAddedToList()
+        {
+            //Arrange
+            Database database = new Database();
+            List<char> TextFileRequiredLetterTest = new List<char>("l");
+            //Act
+            database.ReadTextFile();
+            //Assert 
+            Assert.IsNotNull(database.TextFileRequiredLetter);
+            Assert.AreEqual(TextFileRequiredLetterTest[0], database.TextFileRequiredLetter[0]);
+        }
+        [TestMethod]
+        public void PasswordValuesAreAddedToList()
+        {
+            //Arrange
+            Database database = new Database();
+
+            //Act
+            database.ReadTextFile();
+            //Assert 
+            Assert.IsNotNull(database.TextFilePassword);
+            Assert.AreEqual("vdcv", database.TextFilePassword[0]);
+        }
+    }
+}
