61712ab77d47c9c0584ac3f1b0a0391e58d51e94
Luca Bulles <lucabulles@gmail.com>
2021-01-12 14:30:52 +0100

    Added PasswordPolicy TestClass and added folder for database

---
+++ AdventOfCodeDay2Console.Tests/DoesPasswordPolicyWork.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCodeDay2Console.Tests
+{
+    [TestClass]
+    public class DoesPasswordPolicyWork
+    {
+        [TestMethod]
+        public void DoesPasswordPolicyPartOneWork()
+        {
+            PasswordPolicy passwordPolicy = new PasswordPolicy();
+
+        }
+    }
+}
