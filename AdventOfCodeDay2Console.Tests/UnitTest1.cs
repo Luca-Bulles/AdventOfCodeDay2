926d46a80b87a37d13dba92987bfe15d366a31e1
Luca Bulles <lucabulles@gmail.com>
2021-01-12 13:22:28 +0100

    Added project voor UnitTests

---
+++ AdventOfCodeDay2Console.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCodeDay2Console.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
