057aabea33f84ca9f52074042896630a7e78bf01
Luca Bulles <lucabulles@gmail.com>
2021-01-12 14:47:22 +0100

    removed Arrange Act Assert Comments

--- AdventOfCodeDay2Console.Tests/Database/AreItemsAddedToLists.cs
+++ AdventOfCodeDay2Console.Tests/Database/AreItemsAddedToLists.cs
@@ -11,48 +11,33 @@ namespace AdventOfCodeDay2Console.Tests
         [TestMethod]
         public void MinimalNumberValuesAreAddedToList()
         {
-            //Arrange
             Database database = new Database();
-
-            //Act
             database.ReadTextFile();
-            //Assert 
             Assert.IsNotNull(database.TextFileMinimalNumber);
             Assert.AreEqual(3, database.TextFileMinimalNumber[0]);
         }
         [TestMethod]
         public void MaximalNumberValuesAreAddedToList()
         {
-            //Arrange
             Database database = new Database();
-
-            //Act
             database.ReadTextFile();
-            //Assert 
             Assert.IsNotNull(database.TextFileMaximalNumber);
             Assert.AreEqual(4, database.TextFileMaximalNumber[0]);
         }
         [TestMethod]
         public void RequiredLetterValuesAreAddedToList()
         {
-            //Arrange
             Database database = new Database();
             List<char> TextFileRequiredLetterTest = new List<char>("l");
-            //Act
             database.ReadTextFile();
-            //Assert 
             Assert.IsNotNull(database.TextFileRequiredLetter);
             Assert.AreEqual(TextFileRequiredLetterTest[0], database.TextFileRequiredLetter[0]);
         }
         [TestMethod]
         public void PasswordValuesAreAddedToList()
         {
-            //Arrange
             Database database = new Database();
-
-            //Act
             database.ReadTextFile();
-            //Assert 
             Assert.IsNotNull(database.TextFilePassword);
             Assert.AreEqual("vdcv", database.TextFilePassword[0]);
         }
