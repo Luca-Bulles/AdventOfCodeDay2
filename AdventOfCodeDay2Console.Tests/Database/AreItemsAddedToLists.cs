3b72ec1f4cdfadac1183c19fe707a668f157f8de
Luca Bulles <lucabulles@gmail.com>
2021-01-15 15:29:42 +0100

    Added Commands for Arrange Act and Assert in testmethods

--- AdventOfCodeDay2Console.Tests/Database/AreItemsAddedToLists.cs
+++ AdventOfCodeDay2Console.Tests/Database/AreItemsAddedToLists.cs
@@ -11,33 +11,53 @@ namespace AdventOfCodeDay2Console.Tests
         [TestMethod]
         public void MinimalNumberValuesAreAddedToList()
         {
+            //arrange
             Database database = new Database();
+
+            //act
             database.ReadTextFile();
+
+            //assert
             Assert.IsNotNull(database.TextFileMinimalNumber);
             Assert.AreEqual(3, database.TextFileMinimalNumber[0]);
         }
         [TestMethod]
         public void MaximalNumberValuesAreAddedToList()
         {
+            //arrange
             Database database = new Database();
+
+            //act
             database.ReadTextFile();
+
+            //assert
             Assert.IsNotNull(database.TextFileMaximalNumber);
             Assert.AreEqual(4, database.TextFileMaximalNumber[0]);
         }
         [TestMethod]
         public void RequiredLetterValuesAreAddedToList()
         {
+            //arrange
             Database database = new Database();
             List<char> TextFileRequiredLetterTest = new List<char>("l");
+
+            //act
             database.ReadTextFile();
+
+            //assert
             Assert.IsNotNull(database.TextFileRequiredLetter);
             Assert.AreEqual(TextFileRequiredLetterTest[0], database.TextFileRequiredLetter[0]);
         }
         [TestMethod]
         public void PasswordValuesAreAddedToList()
         {
+            //arrange
             Database database = new Database();
+
+            //act
             database.ReadTextFile();
+
+            //assert
             Assert.IsNotNull(database.TextFilePassword);
             Assert.AreEqual("vdcv", database.TextFilePassword[0]);
         }
--- AdventOfCodeDay2Console.Tests/PasswordPolicy/DoesPasswordPolicyWork.cs
+++ AdventOfCodeDay2Console.Tests/PasswordPolicy/DoesPasswordPolicyWork.cs
@@ -11,16 +11,22 @@ namespace AdventOfCodeDay2Console.Tests
         [TestMethod]
         public void DoesPasswordPolicyPartOneWork()
         {
+            //arrange
             PasswordPolicy passwordPolicy = new PasswordPolicy();
             int correctPasswords = passwordPolicy.PasswordControl();
+
+            //assert
             Assert.AreEqual(447, correctPasswords);
         }
 
         [TestMethod]
         public void DoesPasswordPolicyPartTwoWork()
         {
+            //arrange
             PasswordPolicy passwordPolicy = new PasswordPolicy();
             int correctPasswords = passwordPolicy.PasswordControlPartTwo();
+
+            //assert
             Assert.AreEqual(249, correctPasswords);
         }
     }
